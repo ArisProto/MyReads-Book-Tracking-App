29d0c90aa86810f62e5bda44c9d6e98275fbd578
Aris-Protopapas <aris.protopapas@gmail.com>
2018-08-20 23:39:19 +0200

    fix: got App to a semi working state

--- src/Books.js
+++ src/Books.js
@@ -5,7 +5,7 @@ class Book extends Component {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "" }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: Book.imageLinks ? `url(${Book.imageLinks.thumbnail})` : "" }}></div>
               <div className="book-shelf-changer">
                 <select
                   onChange={(event) => this.props.updateShelf(this.props.book, event.target.value)}
